74ea2d58c444ed7ea019b600d6ebe0cfca87a912
ramvsiva <129750019+ramvsiva@users.noreply.github.com>
2024-06-02 21:56:28 +0200

    Update comments on middleware.js

--- client/src/redux/middleware.js
+++ client/src/redux/middleware.js
@@ -1,14 +1,29 @@
 import axios from "axios";
 
+/**
+ * Redux middleware for handling API requests as part of dispatched actions.
+ * This middleware checks for an 'apiPackage' in the action to determine if it should
+ * process an API request. If 'apiPackage' is present, it will execute the request and
+ * dispatch subsequent actions based on the request's outcome.
+ *
+ * @param {Object} store - The Redux store.
+ * @returns {Function} - Returns a function that handles the next middleware in the chain.
+ */
+
 const apiMiddleware = store => next => action => {
+  // Check if the action should be processed by this middleware
   if (!action.apiPackage) return next(action);
 
+  // Extract details necessary for making the API call from the action
   const { type, apiPackage } = action;
   const { headers, body, method, parameters, query, responseType } = apiPackage;
 
+  // Notify that the API request is starting
   next({
     type: `${type}_PENDING`,
   });
+
+  // Perform the API request using axios
   axios({
     method: method,
     url: parameters,
